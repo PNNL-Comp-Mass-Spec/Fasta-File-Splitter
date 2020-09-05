1f11d907b2ce1136e47c0df0ab9c9fa862d9e686
alchemistmatt <matthew.monroe@pnnl.gov>
2020-09-04 21:29:23 -0700

    Add example batch file and shell script

---
+++ bin/RunProgram.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mono FastaFileSplitter.exe  H_sapiens_IPI_2008-02-07.fasta
+mono FastaFileSplitter.exe  H_sapiens_IPI_2008-02-07.fasta /N:4
---
+++ bin/Run_Program.bat
@@ -0,0 +1,4 @@
+FastaFileSplitter.exe  H_sapiens_IPI_2008-02-07.fasta
+FastaFileSplitter.exe  H_sapiens_IPI_2008-02-07.fasta /N:4
+
+pause
