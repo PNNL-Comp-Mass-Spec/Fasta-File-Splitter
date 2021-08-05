57716c4f66aa189fa9bd8ee31cc582a1f76f3087
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-05 15:21:28 -0700

    Suppress warnings

---
+++ FastaFileSplitterDLL/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Simplification", "RCS1179:Unnecessary assignment.", Justification = "<Pending>", Scope = "member", Target = "~M:FastaFileSplitterLibrary.clsFastaFileSplitter.GetErrorMessage~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:FastaFileSplitterLibrary.clsFastaFileSplitter")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:FastaFileSplitterLibrary.clsFastaOutputFile")]
